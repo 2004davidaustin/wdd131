f1feb035a7d60ad6602533b3fcf2d68159858da4
2004davidaustin <2004davidaustin@gmail.com>
2026-01-14 00:26:23 -0700

    prugie

---
+++ mission/mission.js
@@ -0,0 +1,26 @@
+let selectElem = document.querySelector('#theme-select');
+let pageContent = document.querySelector('body');
+const image = document.querySelector('img');
+
+selectElem.addEventListener('change', changeTheme);
+
+function changeTheme() {
+    let current = selectElem.value;
+    if (current === 'light') {
+        console.log(`yo whaddap`);
+        image.setAttribute('src', 'https://wddbyui.github.io/wdd131/images/byui-logo-blue.webp');
+        document.documentElement.style.setProperty('--background', 'white');
+        document.documentElement.style.setProperty('--text-color', 'black');
+        document.documentElement.style.setProperty('--accent-color', 'navy');
+        document.documentElement.style.setProperty('--border-color', 'darkgrey');
+    } else if (current === 'dark') {
+        image.setAttribute('src', 'https://wddbyui.github.io/wdd131/images/byui-logo-white.png');
+        document.documentElement.style.setProperty('--background', '#2c2c2c');
+        document.documentElement.style.setProperty('--text-color', 'white');
+        document.documentElement.style.setProperty('--accent-color', 'lightblue');
+        document.documentElement.style.setProperty('--border-color', '#888888');
+    } else {
+        // default
+        document.body.style.backgroundImage = "none";
+    }
+}
