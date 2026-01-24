5f5f6a318d18e50a914b1d2258aebbdd1f180f12
2004davidaustin <2004davidaustin@gmail.com>
2026-01-23 18:43:14 -0700

    1/23/2026

--- mission/mission.js
+++ mission/mission.js
@@ -1,23 +1,19 @@
-let selectElem = document.querySelector('#theme-select');
-let pageContent = document.querySelector('body');
-const image = document.querySelector('img');
+// Select DOM elements
+const selectElem = document.querySelector("#theme-select");
+const pageContent = document.querySelector("body");
+const logoImage = document.querySelector("img");
 
-selectElem.addEventListener('change', changeTheme);
+// Add event listener
+selectElem.addEventListener("change", changeTheme);
 
 function changeTheme() {
-    let current = selectElem.value;
-    if (current === 'light') {
-        console.log(`yo whaddap`);
-        image.setAttribute('src', 'https://wddbyui.github.io/wdd131/images/byui-logo-blue.webp');
-        document.documentElement.style.setProperty('--background', 'white');
-        document.documentElement.style.setProperty('--text-color', 'black');
-        document.documentElement.style.setProperty('--accent-color', 'navy');
-        document.documentElement.style.setProperty('--border-color', 'darkgrey');
-    } else if (current === 'dark') {
-        image.setAttribute('src', 'https://wddbyui.github.io/wdd131/images/byui-logo-white.png');
-        document.documentElement.style.setProperty('--background', '#333');
-        document.documentElement.style.setProperty('--text-color', 'white');
-        document.documentElement.style.setProperty('--accent-color', 'lightblue');
-        document.documentElement.style.setProperty('--border-color', '#888888');
-    }
+  const current = selectElem.value;
+
+  if (current === "light") {
+    logoImage.setAttribute("src", "https://wddbyui.github.io/wdd131/images/byui-logo-blue.webp");
+    pageContent.classList.remove("dark");
+  } else if (current === "dark") {
+    logoImage.setAttribute("src", "https://wddbyui.github.io/wdd131/images/byui-logo-white.png");
+    pageContent.classList.add("dark");
+  }
 }
